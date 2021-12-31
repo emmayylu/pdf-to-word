b1a5697ade9a716caf72d2a832c3cc58c64ad758
emmayylu <emma.lu@flashana.com>
2021-12-30 23:55:47 -0700

    decode pdf

--- src/main/java/pdf/to/word/controllers/RootController.java
+++ src/main/java/pdf/to/word/controllers/RootController.java
@@ -1,6 +1,5 @@
 package pdf.to.word.controllers;
 
-import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,8 +15,4 @@ public class RootController {
         return "pong";
     }
 
-
-    public static void main(String[] args) {
-        SpringApplication.run(RootController.class, args);
-    }
 }
--- src/main/java/pdf/to/word/services/Base64Processor.java
+++ src/main/java/pdf/to/word/services/Base64Processor.java
@@ -3,10 +3,12 @@ package pdf.to.word.services;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Base64;
+import java.nio.file.*;
 
-public class DocumentEncoder {
+public class Base64Processor {
     /**
      * Base64 encode a document.
      * @param path Location in which the document to convert is stored.
@@ -17,4 +19,12 @@ public class DocumentEncoder {
         File file = new File(path);
         return Base64.getEncoder().encodeToString(FileUtils.readFileToByteArray(file));
     }
+
+    static String decodePdf(String encodedPdf) throws IOException {
+        byte[] decodedPdf = Base64.getDecoder().decode(encodedPdf);
+        String decodedPdfPath = Files.createTempDirectory("temp") + "/decodedPdf.pdf";
+        FileUtils.writeByteArrayToFile(new File(decodedPdfPath), decodedPdf);
+        return decodedPdfPath;
+    }
+
 }
